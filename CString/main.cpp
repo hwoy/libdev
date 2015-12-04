9a3ca34d530e6b5fa51fad3c0cad65d400e06e3a
Watthanachai Dueanklang <zaintcorp@yahoo.com>
2015-12-05 00:25:27 +0700

    modified:   CString/Makefile 	new file:   CString/function.c 	new file:   CString/function.h 	modified:   CString/main.cpp

--- CString/main.cpp
+++ CString/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "CString.h"
 #include "scvt.h"
+#include "function.h"
 
 using namespace std;
 int main(void)
@@ -18,4 +19,14 @@ int main(void)
 	cout << str << endl;
 	(str="Hello World!")+=" Fuck you!";
 	cout << str << endl;
+
+	str.destroy();
+	str.balloc(32);	
+	ui2s (10001, str.get(), str.getsize(),10, 0);
+	cout << str << endl;
+	
+	str=str+" Dogs";
+	cout << str << endl;
+	
+	return 0;
 }
