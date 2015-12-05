e146c708424f0b0c165040a585ec7986f95db6f5
Watthanachai Dueanklang <zaintcorp@yahoo.com>
2015-12-05 09:21:17 +0700

    modified:   CString/main.cpp 	new file:   README

--- CString/main.cpp
+++ CString/main.cpp
@@ -6,27 +6,106 @@
 using namespace std;
 int main(void)
 {
-	CString str="Hello World!";
+	CString str="Opreation=",str2;
 	cout << str << endl;
-	str+=" My name is Hwoy";
-	cout << str << endl;
-	cout << str.getsize() << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
 	
+	str+=" Opreation+=";
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	
+	str=str+" Opreation+";
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	
+	str2=str="Hello";
+	
+	if(str=="Hello" && str==str2)
+	{
+	str=str+" Opreation==";
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	}
+	
+	str="Hello";
+	
+	if(str!="Hello1")
+	{
+	str=str+" Opreation!=";
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	}
+	
+	
+	cout << "== ui2s function base=10 ==" << endl;
 	str.destroy();
 	str.balloc(32);
 	
+	scvt::ui2s<unsigned int>(str,-1);
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	scvt::ui2s<unsigned short>(str,-1);
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
 	scvt::ui2s<unsigned char>(str,-1);
 	cout << str << endl;
-	(str="Hello World!")+=" Fuck you!";
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	cout << "== ui2s function base=16 ==" << endl;
+	scvt::ui2s<unsigned int>(str,-1,16);
 	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	scvt::ui2s<unsigned short>(str,-1,16);
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	scvt::ui2s<unsigned char>(str,-1,16);
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	cout << "== ui2s function base=2 ==" << endl;
+	
+	scvt::ui2s<unsigned int>(str,-1,2);
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	scvt::ui2s<unsigned short>(str,-1,2);
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
+	
+	scvt::ui2s<unsigned char>(str,-1,2);
+	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
 
-	str.destroy();
+	
+	cout << "== test str=NULL = destroy() ==" << endl;
+	str=NULL;
 	str.balloc(32);	
+	
 	ui2s (10001, str.get(), str.getsize(),10, 0);
 	cout << str << endl;
+	cout << "STR size = " << str.strlen() << endl;
+	cout << "MEM size = " << str.getsize() << endl << endl;
 	
-	str=str+" Dogs";
-	cout << str << endl;
 	
 	return 0;
 }
