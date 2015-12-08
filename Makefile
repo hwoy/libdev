d8bf415413e279a58fe29066ef1f53a49844ad8f
Watthanachai Dueanklang <zaintcorp@yahoo.com>
2015-12-08 23:37:21 +0700

    modified:   Makefile 	modified:   README 	new file:   exam/Makefile 	new file:   exam/prime.cpp

--- Makefile
+++ Makefile
@@ -16,9 +16,13 @@ clean:
 	make -C Copt clean && \
 	make -C Crandom clean && \
 	make -C CString clean && \
-	make -C Cexception clean
+	make -C Cexception clean && \
+	make -C exam clean && \
 	rm -f lib/*.o
 install: all
 	find ./ -name *.o | grep -v main.o | xargs -I {} cp {} lib 
+	
+exam: install
+	make -C exam
 
 
---
+++ exam/prime.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <CString.h>
+#include <_scvt.h>
+#include <Cmsg.h>
+#include <Copt.h>
+
+template <class T>
+class Prime
+{
+protected:
+T num;
+
+public:
+Prime(T num=0);
+void set(T num);
+T get() const;
+bool isPrime();
+static bool isPrime(T num);
+};
+
+template <class T>
+Prime<T>::Prime(T num)
+{
+	this->num=num;
+}
+
+template <class T>
+void Prime<T>::set(T num)
+{
+	this->num=num;
+}
+
+template <class T>
+T Prime<T>::get() const
+{
+	return num;
+}
+
+template <class T>
+bool Prime<T>::isPrime()
+{
+	return isPrime(num);
+}
+
+template <class T>
+bool Prime<T>::isPrime(T num)
+{
+	T i;
+	
+	if(!num) return false;
+	else if(num<=2) return true;
+	
+	for(i=2;i<num;i++)
+	{
+		if(!(num%i)) return false;
+	}
+	
+	return true;
+}
+
+using namespace std;
+
+class Cerror : public base_error
+{
+	public:
+	int show(const char *str,unsigned int index)
+	{
+	cerr << "Error:" << index << ':' << str << ':' << getstr(index) << endl;
+	
+	return -(index+1);
+	}
+};
+class Chelp : public base_help
+{
+	public:
+	int show(int ret=RET)
+{
+	unsigned int i;
+	for(i=0;i<getitem().getNelement();i++)
+		cerr << getitem().getstr(i) << ':' << getitem_desc().getstr(i) << endl;
+	
+	return ret;
+}
+};
+
+class Excep
+{
+	protected:
+	unsigned int errNO;
+	bool isHelp;
+	CString msg;
+	
+	public:
+	Excep(unsigned int errNO,const char *msg,bool isHelp)
+	{
+		this->errNO=errNO;
+		this->msg=msg;
+		this->isHelp=isHelp;
+	}
+	
+	unsigned int geterrNO() const
+	{
+		return errNO;
+	}
+	
+	bool getisHelp() const
+	{
+		return isHelp;
+	}
+	
+	const CString& getmsg() const
+	{
+		return msg;
+	}
+	
+};
+
+
+
+
+static const char *param[]={"-b:","-e:","-h",NULL};
+static const char *param_desc[]={"Begin numer","End number:","-h",NULL};
+enum
+{
+	p_b,p_e,p_h
+};
+
+static const char *errmsg[]={"Too few parameter","Not an Unsigned Number","Begin>End","Invalid an Option",NULL};
+enum
+{
+	err_few,err_nan,err_be,err_inv
+};
+
+typedef unsigned int prime_t;
+
+int main(int argc,const char *argv[])
+{
+	int i;
+	unsigned int count;
+	prime_t j;
+	prime_t begin,end;
+	
+	Prime<prime_t> pm;
+	Cerror err;
+	Chelp help;
+	
+	Copt opt(argc,argv,param);
+	err.setmsg(errmsg);
+	help.setmsg(param,param_desc);
+	
+	begin=end=0;
+	
+	
+	try{
+		
+	if(opt.getargc()<2)
+		throw (Excep(err_few,"argc",true));
+		
+	while((i=opt.action())!=Copt::END)
+	{
+		switch(i)
+		{
+			case p_b:
+			if(!scvt::isUint<Copt>(opt))
+				throw (Excep(err_nan,opt.get(),false));
+			
+			begin=scvt::s2ui<Copt,prime_t>(opt);
+			break;
+			
+			case p_e:
+			if(!scvt::isUint<Copt>(opt))
+				throw (Excep(err_nan,opt.get(),false));
+			
+			end=scvt::s2ui<Copt,prime_t>(opt);
+			break;
+			
+			case p_h:
+			return help.show(1);
+			break;
+			
+			case Copt::OTHER:
+			if(opt.getargc()==2 && scvt::isUint<Copt>(opt))
+			{
+				end=scvt::s2ui<Copt,prime_t>(opt);
+				break;
+			}
+				
+			throw (Excep(err_inv,opt.get(),true));
+			
+		}			
+	}
+	
+	if(begin>end) throw (Excep(err_be,"err_be",false));
+	
+	
+	}catch(Excep &e){ if(e.getisHelp()) {help.show(); cout << endl;}  return err.show(e.getmsg().get(),e.geterrNO());}
+	
+	for(count=0,j=begin;j<=end;j++)
+	{
+		pm.set(j);
+		if(pm.isPrime())
+		{
+			cout << pm.get() << endl;
+			count++;
+		}
+	}
+	
+	cout << endl << begin << " to " << end << " has " << count << endl;
+	
+	return 0;
+}
